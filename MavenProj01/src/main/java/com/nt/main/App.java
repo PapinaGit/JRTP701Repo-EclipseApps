1e54f34650a8f5c9f6fc0eb0b407df8840038ed1
PapinaGit <papinarana54@gmail.com>
2025-06-26 12:27:16 +0530

    TL modified the App.java file

--- MavenProj01/src/main/java/com/nt/main/App.java
+++ MavenProj01/src/main/java/com/nt/main/App.java
@@ -4,7 +4,26 @@ package com.nt.main;
  * Hello world!
  */
 public class App {
+	
+	public int sum(int a, int b) {
+		return a+b;
+	}
+	
+	public int sub(int x, int y) {
+		return x-y;
+	}
+	
+	public int mul(int p, int q) {
+		return p*q;
+	}
+	
+	
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        App app = new App();
+        System.out.println("sum is:: "+app.sum(10, 20));
+        System.out.println("sub is:: "+app.sum(20, 10));
+        System.out.println("mul is:: "+app.sum(10, 20));
     }
 }
