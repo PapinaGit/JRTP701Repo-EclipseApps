b18d65f2a2cd9ea29daaf09959a2279c5364d154
PapinaGit <papinarana54@gmail.com>
2025-06-26 15:15:39 +0530

    Added NetBanking by Dev1

---
+++ MavenProj01/src/main/java/com/nt/payment/NetBanking_Payment.java
@@ -0,0 +1,9 @@
+package com.nt.payment;
+
+public class NetBanking_Payment {
+	
+	public String doPayment(long accNo, long cardNo) {
+		System.out.println("NetBanking is done");
+		return "NetBanking is done.";
+	}	
+}
