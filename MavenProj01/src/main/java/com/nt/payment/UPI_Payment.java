4ddaf1661ca62a3b495d4abd2ff63e6b88e1c2dc
PapinaGit <papinarana54@gmail.com>
2025-06-26 15:05:51 +0530

    TL- created Payment Branch and added UPI_Payment class

---
+++ MavenProj01/src/main/java/com/nt/payment/UPI_Payment.java
@@ -0,0 +1,10 @@
+package com.nt.payment;
+
+public class UPI_Payment {
+	
+	public String doPayment(long phoneNo, double amt) {
+		System.out.println("Payment is done");
+		return "Payment is done";
+	}
+
+}
